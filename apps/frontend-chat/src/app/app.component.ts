4651a9ec90508bc44759827a2bb320d4ceacb128
sanjay <sanjay139474@gmail.com>
2025-04-27 13:20:33 +0530

    setup project

--- apps/frontend-chat/src/app/app.component.ts
+++ apps/frontend-chat/src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { ReactiveFormsModule, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'frontend-chat';
+  form!: FormArray;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.array([
+      {
+        name: this.fb.group({
+          lastName: this.fb.control(''),
+        }),
+      },
+    ]);
+
+    console.log(this.form);
+  }
 }
