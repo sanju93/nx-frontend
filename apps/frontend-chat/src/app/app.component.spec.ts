ba7b341aee5ca26b0961f290e0874bde9c62642c
sanjay <sanjay139474@gmail.com>
2025-04-27 13:29:36 +0530

    fix test

--- apps/frontend-chat/src/app/app.component.spec.ts
+++ apps/frontend-chat/src/app/app.component.spec.ts
@@ -14,9 +14,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome frontend-chat'
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('hello');
   });
 
   it(`should have as title 'frontend-chat'`, () => {
