3839933f22c93c610aeb3773f496b5fa5a21a226
sanjay <sanjay139474@gmail.com>
2025-04-28 23:43:53 +0530

    code update

--- apps/frontend-chat/src/app/app.component.ts
+++ apps/frontend-chat/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   imports: [RouterModule, ReactiveFormsModule],
