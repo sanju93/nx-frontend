c4c2b7373c6e8930432aeb1eb22c54ce68cc38dd
sanjay <sanjay139474@gmail.com>
2025-05-12 19:11:54 +0530

    code update

--- apps/frontend-chat/src/app/app.routes.ts
+++ apps/frontend-chat/src/app/app.routes.ts
@@ -1,10 +1,6 @@
 import { Route } from '@angular/router';
-import { SharedComponent } from '@frontend-chat/Shared';
+
 
 export const appRoutes: Route[] = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('@frontend-chat/Shared').then((c) => c.SharedComponent),
-  },
+ 
 ];
--- libs/Shared/README.md
+++
@@ -1,7 +0,0 @@
-# Shared
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test Shared` to execute the unit tests.
