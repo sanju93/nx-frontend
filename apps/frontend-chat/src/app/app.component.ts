9899bb129a533a8a2cc9bb64d77b931947892570
sanjay <sanjay139474@gmail.com>
2025-05-12 20:09:59 +0530

    setup formly form in project

--- apps/frontend-chat/src/app/app.component.ts
+++ apps/frontend-chat/src/app/app.component.ts
@@ -1,19 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
-import { appRoutes } from './app.routes';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+
 import { TextareaComponent } from '@frontend-chat/textarea';
+import { FormlyFieldConfig, FormlyModule } from '@ngx-formly/core';
 
 @Component({
-  imports: [RouterModule, ReactiveFormsModule, TextareaComponent],
+  imports: [RouterModule, FormlyModule, ReactiveFormsModule, TextareaComponent],
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'frontend-chat';
 
-  form: any;
+  form = new FormGroup({});
+  model = { name: 'sanjay' };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'name',
+      type: 'input',
+      templateOptions: {
+        label: 'Name',
+        placeholder: 'Enter the name ',
+        required: true,
+      },
+    },
+  ];
 
   constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    console.log(this.form.controls);
+  }
 }
