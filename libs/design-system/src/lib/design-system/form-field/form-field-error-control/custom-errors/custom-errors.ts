d69b38b2ac833881d356f51d32cde4fe64a43418
sanjay <sanjay139474@gmail.com>
2025-06-16 16:53:27 +0530

    updated error form control

--- libs/design-system/src/lib/design-system/form-field/form-field-error-control/custom-errors/custom-errors.ts
+++ libs/design-system/src/lib/design-system/form-field/form-field-error-control/custom-errors/custom-errors.ts
@@ -2,6 +2,7 @@ import { InjectionToken } from '@angular/core';
 
 export const CUSTOM_ERRORS = new InjectionToken('FormControlErrors');
 
-export const errors: Record<string, ([...args]) => string> = {
+export const errors: Record<string, any> = {
   required: () => 'Field is Required',
+  email: () => 'Please input a correct email address',
 };
--- libs/design-system/src/lib/design-system/form-field/form-field-error-control/form-field-error-control.component.ts
+++ libs/design-system/src/lib/design-system/form-field/form-field-error-control/form-field-error-control.component.ts
@@ -1,27 +1,49 @@
-import { Component, Inject, input } from '@angular/core';
-import { AbstractControl } from '@angular/forms';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Inject,
+  input,
+} from '@angular/core';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
 import { CUSTOM_ERRORS, errors } from './custom-errors/custom-errors';
+import { debounceTime, pipe, Subject, takeUntil } from 'rxjs';
+import { FormFieldErrorMessageComponent } from './form-field-error-message/form-field-error-message.component';
 
 @Component({
   selector: 'mat-form-field-error-control',
   templateUrl: './form-field-error-control.component.html',
   styleUrls: ['./form-field-error-control.component.scss'],
+  imports: [FormFieldErrorMessageComponent],
   providers: [
     {
       provide: CUSTOM_ERRORS,
       useValue: errors,
     },
   ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormFieldErrorControlComponent {
-  errorMessagesMap = input<Record<string, () => string>>();
+  errorMessagesMap = input<Record<string, any>>();
   errorFormControl = input<AbstractControl | null>();
   private _errorFormControl!: AbstractControl | null;
-  constructor(
-    @Inject(CUSTOM_ERRORS) private errors: Record<string, () => string>
-  ) {
-    console.log(this.errors);
+
+  errorMessages: string[] = [];
+
+  unsubscribe$ = new Subject<void>();
+
+  get inValid(): boolean {
+    return (
+      (this._errorFormControl &&
+        this._errorFormControl.touched &&
+        this._errorFormControl.invalid) ??
+      false
+    );
   }
+  constructor(
+    @Inject(CUSTOM_ERRORS) private errors: Record<string, () => string>,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     this._errorFormControl = this.errorFormControl() ?? null;
@@ -29,8 +51,48 @@ export class FormFieldErrorControlComponent {
   }
 
   listenForFormControlStatus() {
-    this._errorFormControl?.statusChanges?.subscribe((status) => {
-      console.log(status);
-    });
+    this._errorFormControl?.statusChanges
+      ?.pipe(debounceTime(200), takeUntil(this.unsubscribe$))
+      .subscribe(() => {
+        this.getErrors();
+      });
+  }
+
+  getErrors() {
+    if (!this._errorFormControl) return;
+    const controlErrors: ValidationErrors | null =
+      this._errorFormControl.errors;
+
+    if (controlErrors) {
+      if (this.inValid) {
+        const firstKey = Object.keys(controlErrors)[0];
+        const errorMap = this.getErrorMessage(firstKey);
+
+        this.errorMessages = [
+          errorMap ? errorMap(controlErrors[firstKey]) : 'Unexpected Error',
+        ];
+        console.log(this.errorMessages);
+      } else {
+        this.errorMessages = [];
+      }
+    } else {
+      this.errorMessages = [];
+    }
+
+    this.cdr.markForCheck();
+  }
+
+  getErrorMessage(key: string) {
+    if (this.errorMessagesMap()) {
+      const messageMap = this.errorMessagesMap();
+      return messageMap && messageMap[key];
+    } else {
+      return this.errors[key];
+    }
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
